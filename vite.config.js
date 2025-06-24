195c744452ad6f9674d660569611bbdadb891657
DVLPJosh <guerrajoshua000@gmail.com>
2025-06-24 18:05:54 +0800

    ebuild with correct base path

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,4 +5,3 @@ export default defineConfig({
   base: '/Web-app/',
   plugins: [react()],
 })
-
